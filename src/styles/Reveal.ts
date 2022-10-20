c83b37394462090821e23f0da031f50da8be8a3c
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-21 01:15:28 +0900

    delete dead code

--- src/styles/Reveal.ts
+++ src/styles/Reveal.ts
@@ -1,18 +1,6 @@
-import styled, { keyframes } from 'styled-components'
+import { keyframes } from 'styled-components'
 
 export const RevealFromTop = keyframes`
-  100% {
-    height: 100%;
-  }
-`
-
-export const RevealFromLeft = keyframes`
-  100% {
-    width: 100%;
-  }
-`
-
-export const HiddenFromBtm = keyframes`
   0% {
     height: 0%;
   }
@@ -21,16 +9,8 @@ export const HiddenFromBtm = keyframes`
   }
 `
 
-export const RevealWrap = styled.div`
-  position: relative;
-  width: fit-content;
-  height: fit-content;
-
-  &:before {
-    content: '';
-    position: absolute;
+export const RevealFromLeft = keyframes`
+  100% {
     width: 100%;
-    height: 100%;
-    background: ${({ theme }) => theme.colors.black};
   }
 `
