87f2a12f9119b20c833819fd9ac2844979ae5d09
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-10-21 01:33:59 +0900

    update WorkTitle Mobile

--- src/components/atoms/text/WorkTitle.tsx
+++ src/components/atoms/text/WorkTitle.tsx
@@ -85,7 +85,7 @@ const _MobileTitleWrap = styled.div<{ inView: boolean }>`
     bottom: 0;
     left: 0;
     width: 100%;
-    height: 100%;
+    height: 103%;
     background: #000;
     ${(props) =>
       props.inView &&
